9d8871bd4c2b0fa96708cedb1cd15fe94a6dde6b
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2023-04-16 10:11:14 -0300

    Update: Creating and manipulating DB with sequelize

--- src/db/strategys/postgres.js
+++ src/db/strategys/postgres.js
@@ -1,21 +1,74 @@
 const ICrud = require('./interfaces/InterfaceCrud')
-class Postgres extends ICrud{
-    constructor(){
+const Sequelize = require('sequelize')
+class Postgres extends ICrud {
+    constructor() {
         super()
+        this._driver = null
+        this._herois = null
+        this._connect()
     }
-    isConnected(){
-        
+
+    async isConnected() {
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.log('Fail!', error)
+            return false
+        }
     }
-    create(item){
+
+    async defineModel() {
+        this._herois = driver.define('heroes', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.INTEGER,
+                required: true
+            },
+            poder: {
+                type: Sequelize.INTEGER,
+                required: true
+            }
+        },
+            {
+                tableName: 'TB_HEROES',
+                freezTableName: false,
+                timestamps: false
+            })
+    }
+
+    _connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'vinia6',
+            'senha',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )
+    }
+
+    create(item) {
         console.log("O item foi salvo no Postgres")
     }
-    read(item){
+
+    read(item) {
         console.log("Os items do Postgres estão a seguir")
     }
-    update(id,item){
+
+    update(id, item) {
         console.log("O item foi alterado no Postgres")
     }
-    delete(id){
+
+    delete(id) {
         console.log("O item foi deletado no Postgres")
     }
 }
