2505bc7d3e85b26c68d294073b00e43d7bb9009e
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2023-05-06 18:41:50 -0300

    Update: New nethods Http

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const routes = require('./routes/heroRoutes')
 
-
-
 async function main() {
+
     const app = express()
     const port = 5050
-    
+
     app.use('/',routes)
+    app.use('/herois',routes)
+    // app.use('/herois/:id',routes)
 
     app.listen(port, () => {
         console.log(`Listening on port ${port}`)
@@ -15,6 +16,5 @@ async function main() {
 
     return app
 }
-
 
 module.exports = main()
--- src/routes/heroRoutes.js
+++ src/routes/heroRoutes.js
@@ -1,21 +1,71 @@
 const express = require('express');
-const router = express.Router();
-const Context = require('.././db/strategys/base/contextStrategy')
-const MongoDb = require('.././db/strategys/mongodb/mongodb')
-const HeroiSchema = require('.././db/strategys/mongodb/schemas/heroisSchema')
+const Context = require('.././db/strategys/base/contextStrategy');
+const MongoDb = require('.././db/strategys/mongodb/mongodb');
+const HeroiSchema = require('.././db/strategys/mongodb/schemas/heroisSchema');
 
-const connection = MongoDb.connect()
-const context = new Context(new MongoDb(connection, HeroiSchema))
-context.isConnected()
+
+const router = express.Router();
+const connection = MongoDb.connect();
+const context = new Context(new MongoDb(connection, HeroiSchema));
+context.isConnected();
+
+
+router.post('/herois',express.json() ,async (req, res) => {
+    try {
+        
+        const data = await context.create(req.body)
+        res.json(data)
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro no cadastro.');
+    }
+})
 
 router.get('/', async (req, res) => {
     try {
-        const data = await context.read()
-        res.json(data)
+        const data = await context.read();
+        res.json(data);
     } catch (error) {
-        console.error(err);
+        console.error(error);
         res.status(500).send('Erro ao buscar dados!');
     }
 })
+
+
+router.get('/herois', async (req, res) => {
+    try {
+        const { skip, limit } = req.query;
+        const data = await context.read({}, parseInt(skip), parseInt(limit));
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao buscar dados!');
+    }
+})
+
+// router.post('/herois/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         let data = await context.read({ nome: id });
+
+//         if (!data) {
+//             res.status(404).send('Usuário não encontrado.');
+//             return;
+//         }
+
+//         console.log(data)
+
+//         let data_update = req.body;
+
+//         console.log(data_update);
+
+//         // res.send(data_update);
+
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Erro na atualização do dado!');
+//     }
+// })
 
 module.exports = router;
